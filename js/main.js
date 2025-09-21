82aa4f94e02c90844057c3bf9d636fffc4378abc
jdlb-y155 <y155.jdlb@gmail.com>
2025-09-21 12:06:02 -0500

    Musica

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+
+function reproducirAudio() {
+  const audio = document.getElementById("miAudio");
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+};
